480256ca2fa9ffbbabc8a4ae92a4d63dd548a041
Jagy <jagy.das12@gmail.com>
2021-09-25 12:25:13 -0400

    final code check-in

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 
 var timeEl = document.querySelector("p.timer");
 console.log("timeEL" + timeEl);
- var timeLeft = 60;
-var scoreEl =document.querySelector("#score");
+var timeLeft = 60;
+var scoreEl = document.querySelector("#score");
 console.log("scoreEl" + scoreEl);
 
-var introEl =document.querySelector("#intro");
+var introEl = document.querySelector("#intro");
 console.log("introEl" + introEl);
 
 // question section
@@ -24,10 +24,10 @@ var questionAnsCount = 0;
 var finalScoreEl = document.querySelector("#finalScore");
 
 // correct ans check
-  var finalcheckEl = document.querySelector("#finalcheck");
+var finalcheckEl = document.querySelector("#finalcheck");
 //user initials
 
-var initialsEl =document.querySelector("#initials");
+var initialsEl = document.querySelector("#initials");
 
 //higest score(
 
@@ -47,7 +47,7 @@ var quizBtnEl = document.querySelector("#start");
 
 //answer button class
 
-var ansBtnEl =document.querySelectorAll("button.ansBtn");
+var ansBtnEl = document.querySelectorAll("button.ansBtn");
 var ansBtn1El = document.querySelector("#answer1");
 var ansBtn2El = document.querySelector("#answer2");
 var ansBtn3El = document.querySelector("#answer3");
@@ -65,123 +65,125 @@ var clearScrBtn = document.querySelector("#clearscores");
 var viewscoreEl = document.querySelector("#view-score");
 
 // decalre Question object
- //array of objects 
+//array of objects 
 var questions = [{
 
-    question:"Inside which HTML element do we put the JavaScript?",
-    answers: ["1. <javascript>","2. <scripting>","3. <js>","4. <script>"],
-    correctAnswer: "4. <script>"
-},
-{
- question:"Where is the correct place to insert a JavaScript?",
- answers:[" 1. Both the <head> section and the <body> section are correct","2. The <head> section", "3.The <body> section","4. footer section"],
- correctAnswer:" 1. Both the <head> section and the <body> section are correct"
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["1. <javascript>", "2. <scripting>", "3. <js>", "4. <script>"],
+        correctAnswer: 4
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [" 1. Both the <head> section and the <body> section are correct", "2. The <head> section", "3.The <body> section", "4. footer section"],
+        correctAnswer: 1
 
-},
+    },
 
-{
- question: "How can you add a comment in a JavaScript?",
- answers: ["1. This is a comment","2. This is a comment //","3. <!-- This is a comment -->", "4. This is a comment /*"],
- correctAnswer: "3. <!-- This is a comment -->"
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: ["1. This is a comment", "2. This is a comment //", "3. <!-- This is a comment -->", "4. This is a comment /*"],
+        correctAnswer: 3
 
-},
+    },
 
-{
-    question: "Which event occurs when the user clicks on an HTML element?",
-    answers:["1. onclick","2. onmouseover", "3. onchange","4. onmouseclick"],
-    correctAnswer: "4. onmouseclick"
-}
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: ["1. onclick", "2. onmouseover", "3. onchange", "4. onmouseclick"],
+        correctAnswer: 4
+    }
 ];
 
 // Timer function
- function setTimer(){
+function setTimer() {
 
-    var timeInterval= setInterval(function(){
+    var timeInterval = setInterval(function() {
 
         timeLeft--;
         timeEl.textContent = `Time:${timeLeft}s`;
-        if(timeLeft == 0 || questionAnsCount == questions.length){
+        if (timeLeft == 0 || questionAnsCount == questions.length) {
             clearInterval(timeInterval);
             quizQsEl.style.display = "none";
             scoreEl.textContent = timeLeft;
-            finalScoreEl.style.display ="block";
+            finalScoreEl.style.display = "block";
         }
 
-    },1000);
- }
+    }, 1000);
+}
 
 //start quiz with timer
 
-function quizStart(){
-    introEl.style.display ="none";
+function quizStart() {
+    introEl.style.display = "none";
     quizQsEl.style.display = "block";
-    questionAnsCount =0;
+    questionAnsCount = 0;
     setTimer();
     setQuestion(questionAnsCount);
 }
 
 
 // set qs,save the count and display next qs
-    function setQuestion(id){
-       if(id < questions.length){
+function setQuestion(id) {
+    if (id < questions.length) {
         questionsEl.textContent = questions[id].question;
         ansBtn1El.textContent = questions[id].answers[0];
         ansBtn2El.textContent = questions[id].answers[1];
         ansBtn3El.textContent = questions[id].answers[2];
         ansBtn4El.textContent = questions[id].answers[3];
 
-       }
-
     }
+
+}
 
 
 // function to check the answer and display next question
-        function checkAns(event){
-            event.preventDefault();
-        
-
-// append msg to div showing correct and wrong answer count
-finalcheckEl.style.display = "block";
- var p =document.createElement("p");
- finalcheckEl.appendChild(p);
-
- //timout after 1sec
-
- setTimeout(function(){
-    p.style.display ="none";
-
- },1000);
- console.log (event.target.value);
-// answer check
- if(questions[questionAnsCount].correctAnswer == event.target.value){
-
-    p.textContent ="Correct!";
- }
- else if (questions[questionAnsCount].correctAnswer != event.target.value){
-    timeLeft = timeLeft -10;
-    p.textContent = "Wrong!";
- }
-  //increment so question index is increased
-  if(questionAnsCount < questions.length){
-    questionAnsCount++;
-  }
-  // call setQuestion to bring in next question when any ansBtn is clicked
-  setQuestion(questionAnsCount);
+function checkAns(event) {
+    event.preventDefault();
 
 
-  }
- function addScore(event){
-     event.preventDefault();
-     finalScoreEl.style.display = "none";
+    // append msg to div showing correct and wrong answer count
+    finalcheckEl.style.display = "block";
+    var p = document.createElement("p");
+    finalcheckEl.appendChild(p);
+
+    //timout after 1sec
+
+    setTimeout(function() {
+        p.style.display = "none";
+
+    }, 1000);
+    console.log(event.target.value);
+    // answer check
+    if (questions[questionAnsCount].correctAnswer == event.target.value) {
+
+        p.textContent = "Correct!";
+    } else if (questions[questionAnsCount].correctAnswer != event.target.value) {
+        timeLeft = timeLeft - 10;
+        p.textContent = "Wrong!";
+    }
+    //increment so question index is increased
+    if (questionAnsCount < questions.length) {
+        questionAnsCount++;
+    }
+    // call setQuestion to bring in next question when any ansBtn is clicked
+    setQuestion(questionAnsCount);
+
+
+}
+
+function addScore(event) {
+    event.preventDefault();
+    finalScoreEl.style.display = "none";
     highestScoreEl.style.display = "block";
     var init = initialsEl.value.toUpperCase();
     var array = JSON.parse(localStorage.getItem("scoreArr")) || [];
-    array.push({initials: init, score: timeLeft});
+    array.push({ initials: init, score: timeLeft });
 
-localStorage.setItem("scoreArr", JSON.stringify(array));
+    localStorage.setItem("scoreArr", JSON.stringify(array));
 
- }
- //sorted score
+    showHighScore();
+}
+
+//sorted score
 //  scoreArr = scoreArr.sort((a,b) =>{
 //      if(a.score < b.score){
 //          return 1;
@@ -189,18 +191,19 @@ localStorage.setItem("scoreArr", JSON.stringify(array));
 //          return -1 ;
 //      }
 //  }
- 
- //);
 
- scoreListEl.innerHTML = "";
- for(var i =0; i < scoreArr.length; i++){
-  var li = document.createElement("li");
-  li.textContent = `${scoreArr[i].initials}:``${scoreArr[i].score}`;
- scoreListEl.append(li);
-   
- }
+//);
 
- // local storage
+scoreListEl.innerHTML = "";
+for (var i = 0; i < scoreArr.length; i++) {
+    var li = document.createElement("li");
+    li.textContent = `${scoreArr[i].initials}:`
+    `${scoreArr[i].score}`;
+    scoreListEl.append(li);
+
+}
+
+// local storage
 //  storeScores();
 
 
@@ -208,9 +211,9 @@ localStorage.setItem("scoreArr", JSON.stringify(array));
 //      localStorage.setItem("scoreArr",JSON.stringify(scoreArr));
 //  }
 
- showHighScore();
+// showHighScore();
 
-  
+
 //  function displayScores() {
 //     // Get stored scores from localStorage
 //     // Parsing the JSON string to an object
@@ -220,12 +223,13 @@ localStorage.setItem("scoreArr", JSON.stringify(array));
 //          scoreArr =storedScoreList;
 //      }
 
- //}
+//}
 
- // clear scores
+// clear scores
 function clearScores() {
     localStorage.clear();
-    scoreListEl.innerHTML="";
+    scoreListEl.innerHTML = "";
+
 }
 
 // EventListeners
@@ -233,47 +237,63 @@ function clearScores() {
 quizBtnEl.addEventListener("click", quizStart);
 
 
-ansBtnEl.forEach(item =>{
-    item.addEventListener("click",checkAns);
+ansBtnEl.forEach(item => {
+    item.addEventListener("click", checkAns);
 });
 //add score
-submitScrBtn.addEventListener("click",addScore);
+submitScrBtn.addEventListener("click", addScore);
 
 //go back button
-goBackBtn.addEventListener("click",function(){
+goBackBtn.addEventListener("click", function() {
     highestScoreEl.style.display = "none";
     introEl.style.display = "block";
-    timeLeft =60;
+    timeLeft = 60;
     timeEl.textContent = `Time:$(timeLeft)s`;
 
 });
 
 // Clear the scores
-clearScrBtn.addEventListener("click",clearScores);
+clearScrBtn.addEventListener("click", clearScores);
 
-// highscore button
-// viewscoreEl.addEventListener("click",function(){
-//     if (highscoresEl.style.display === "none") {
-//         highscoresEl.style.display = "block";
-//     } else if (highscoresEl.style.display === "block") {
-//         highscoresEl.style.display = "none";
-//     } else {
-//         return alert("No scores to show.");
-//     }
-// });
+//highscore button
+viewscoreEl.addEventListener("click", function() {
+
+    showHighScore();
+    if (highestScoreEl.style.display === '') {
+        highestScoreEl.style.display = "block";
+        document.querySelector("#intro").innerHTML = "";
+    }
+    // else if (highestScoreEl.style.display === "block") {
+    //     highestScoreEl.style.display = "none";
+    // } else {
+    //     return alert("No scores to show.");
+    // }
+});
 
 // showing the highscore
 
-  function showHighScore(){
-   
+function showHighScore() {
+    scoreListEl.innerHTML = "";
     var highScoreArray = JSON.parse(localStorage.getItem("scoreArr")) || [];
-    console.log (highScoreArray);
-    for(var i = 0; i < highScoreArray.length; i++){
+    console.log(highScoreArray);
+    //sorted score
+    highScoreArray = highScoreArray.sort((x, y) => {
+            console.log("x" + x + " " + "y" + y);
+            if (x.score < y.score) {
+
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+    );
+
+    for (var i = 0; i < highScoreArray.length; i++) {
         let li = document.createElement("li");
         li.textContent = `${highScoreArray[i].initials}: ${highScoreArray[i].score}`;
         scoreListEl.append(li);
-        
+
     }
 
-  }
-  
+}
